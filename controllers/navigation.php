d0d1ebd68dada7b32a41c8cc060379687b689c8b
Christophe.KUNZLI <Christophe.KUNZLI@cpnv.ch>
2022-11-25 10:57:23 +0100

    initial commit

---
+++ controllers/navigation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file     navigation.php
+ * @brief    File description
+ * @author   Created by Christophe.KUNZLI
+ * @version  24.11.2022
+ */
+function home(): void
+{
+    require("views/home.php");
+}
+
+function lost(): void
+{
+    require("views/lost.php");
+}
---
+++ views/content/lost.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file     lost.php
+ * @brief    File description
+ * @author   Created by Christophe.KUNZLI
+ * @version  24.11.2022
+ */
+echo "<h2>I am so lost</h2>";
